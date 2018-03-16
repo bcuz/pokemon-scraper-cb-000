99362bde2ca84f4c3f0bfc3a3f75f5566f30d832
adam n <adam.nawroc@gmail.com>
2018-03-16 16:09:24 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,8 @@ class Pokemon
     from = db.execute("select * from pokemon where id = #{id}")
     # p from
     # use indexes?
-    return self.new(id: from[0], name: from[1], type: from[2], db: from[3])
+    x = self.new(id: from[0], name: from[1], type: from[2], db: from[3])
+    
 
   end
 end
