88678490d35550d2f352ae14756b06e35707ecd7
adam n <adam.nawroc@gmail.com>
2018-03-16 16:34:00 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,9 +26,9 @@ class Pokemon
 
     # expected 0 ?? dont think i was getting that error b4
     x = self.new(id: from[0], name: from[1], type: from[2], db: db)
+    # # binding.pry
     x
     # x = self.new(from[0], from[1], from[2], db)
-    # # binding.pry
     # x
 
   end
