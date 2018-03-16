859ee1248e5e9ab8d969ed43b5ffe0b12b3fc0f7
adam n <adam.nawroc@gmail.com>
2018-03-16 17:19:55 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -27,6 +27,7 @@ describe "Pokemon" do
       pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
       # edited
       expect(pikachu_from_db).to eq([[1, "Pikachu", "electric", 60]])
+      expect(pikachu_from_db).to eq([[1, "Pikachu", "electric"]])
     end
   end
 
